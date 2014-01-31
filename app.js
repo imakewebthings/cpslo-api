b584b76f81a9dc3899a77ca72d73d79490a3866b
imakewebthings <caleb@imakewebthings.com>
2014-01-30 23:27:34 -0800

    unit endoint, unit courses endpoint

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 var catalog = require('cpslo-catalog');
+var _ = require('lodash');
 
 app.get('/courses/:id', function(req, res) {
   var course = catalog.courses[req.params.id];
   res.send(course ? 200 : 404, course);
+});
+
+app.get('/units/:prefix', function(req, res) {
+  var unit = catalog.units[req.params.prefix];
+  res.send(unit ? 200 : 404, unit);
+});
+
+app.get('/units/:prefix/courses', function(req, res) {
+  var unit = catalog.units[req.params.prefix];
+  if (!unit) return res.send(404);
+  res.send(200, _.select(catalog.courses, function(course) {
+    return _.contains(unit.courses, course.id);
+  }));
 });
 
 module.exports = app;
--- test/api_spec.js
+++ test/api_spec.js
@@ -21,13 +21,57 @@ describe('CPSLO Catalog API', function() {
     });
 
     describe('with invalid id', function() {
-      before(function() {
+      beforeEach(function() {
         this.response = request.get('/courses/csc-000');
       });
 
       it('404s', function(done) {
         this.response.expect(404, done);
       });
+    });
+  });
+
+  describe('GET /units/:prefix', function(done) {
+    describe('with a valid prefix', function() {
+      beforeEach(function() {
+        this.response = request.get('/units/csc');
+      });
+
+      it('200s', function(done) {
+        this.response.expect(200, done);
+      });
+
+      it('returns the unit object', function(done) {
+        this.response.expect(function(res) {
+          if (res.body['title'] !== 'Computer Science') {
+            return 'prefix does not match';
+          }
+        }).end(done);
+      });
+    });
+
+    describe('with invalid id', function() {
+      beforeEach(function() {
+        this.response = request.get('/units/xxx');
+      });
+
+      it('404s', function(done) {
+        this.response.expect(404, done);
+      });
+    });
+  });
+
+  describe('GET /units/:prefix/courses', function() {
+    beforeEach(function() {
+      this.response = request.get('/units/chin/courses');
+    });
+
+    it('returns an array of courses', function(done) {
+      this.response.expect(function(res) {
+        if (!res.body[0].description) {
+          return 'Empty course array returned'
+        }
+      }).end(done);
     });
   });
 });
